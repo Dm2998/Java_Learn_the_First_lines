2e3fb23108e94452d29422a86721fd8c500fe13f
Diego Martin <x00191146@mytudublin.ie>
2022-12-30 13:56:22 +0000

    just  writing  the  first lines of code.

---
+++ java1.java
@@ -0,0 +1,108 @@
+
+// basic in java lenguage for beginners
+// Java is a programming language and computing platform first released by Sun Microsystems in 1995. 
+//There are lots of applications and websites that will not work unless you have Java installed, 
+//and more are created every day. Java is fast, secure, and reliable. From laptops to datacenters, 
+//game consoles to scientific supercomputers, cell phones to the Internet, Java is everywhere!.
+// basic in java lenguage for beginners
+// just for learn another lenguage and practice my skills in programming
+// this is a basic code for learn java lenguage
+// it is a long code but it is very simple for understand the basic of java
+// java is a lenguage easy to learn that python. 
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+public class imput {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello " + name);
+    }
+}
+
+public class address {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Enter your address: ");
+        String address = scanner.nextLine();
+        System.out.println("Hello " + name + " your address is " + address);
+    }
+}
+
+public class age {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Enter your age: ");
+        int age = Integer.valueOf(scanner.nextLine());
+        System.out.println("Hello " + name + " your age is " + age);
+    }
+}
+
+public class country {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Enter your country: ");
+        String country = scanner.nextLine();
+        System.out.println("Hello " + name + " your country is " + country);
+    }
+
+}
+
+
+public class university{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Enter your university: ");
+        String university = scanner.nextLine();
+        System.out.println("Hello " + name + " your university is " + university);
+    }
+
+}
+
+public class python, java, linux, perl, javascript, database {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Enter your python: ");
+        String python = scanner.nextLine();
+        System.out.println("Enter your java: ");
+        String java = scanner.nextLine();
+        System.out.println("Enter your linux: ");
+        String linux = scanner.nextLine();
+        System.out.println("Enter your perl: ");
+        String perl = scanner.nextLine();
+        System.out.println("Enter your javascript: ");
+        String javascript = scanner.nextLine();
+        System.out.println("Enter your database: ");
+        String database = scanner.nextLine();
+        System.out.println("Hello " + name + " your python is " + python + " your java is " + java + " your linux is " + linux + " your perl is " + perl + " your javascript is " + javascript + " your database is " + database);
+    }
+
+}
+
+public class Wellcome Github {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Enter your github: ");
+        String github = scanner.nextLine();
+        System.out.println("Hello " + name + " your github is " + github);
+    }
+
+}
